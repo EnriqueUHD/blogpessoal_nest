1b98c7bd3c84c7e832115a61e04588cd2efe3213
Andreazza <enrique0801@gmail.com>
2025-12-04 20:24:26 -0300

    feature/tema-crud

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
- 
+
 async function bootstrap() {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-member-access
   require('dotenv').config();
   const app = await NestFactory.create(AppModule);
- 
+
   process.env.TZ = '-03:00';
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
- 
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
- 
